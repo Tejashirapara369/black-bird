33754907636d4886971f7ad37197e42a11a57c14
Tejas Hirpara <tejashirapara369@gmail.com>
2022-06-29 04:12:38 -0400

    admin panel left navbar added

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'black-bird-root',
+  selector: 'admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
